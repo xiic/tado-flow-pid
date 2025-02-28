67856988f0c869eccaa2360999cbf8d925591bce
Hinrich Harms <git@hinrich.de>
2025-02-28 23:18:21 +0100

    Retrieve all rooms and start a PID per room

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from dotenv import load_dotenv
 from PyTado.interface.interface import Tado
@@ -13,9 +13,19 @@ load_dotenv()
 
 TADO_USERNAME = os.getenv('TADO_USERNAME')
 TADO_PASSWORD = os.getenv('TADO_PASSWORD')
-FLOW_MIN = 20
-FLOW_MAX_MINUS10 = 70
-FLOW_MAX_PLUS20 = 45
+FLOW_MIN = os.getenv('FLOW_MIN', 20)
+FLOW_MAX_MINUS10 = os.getenv('FLOW_MAX_MINUS10', 75)
+FLOW_MAX_PLUS20 = os.getenv('FLOW_MAX_PLUS20', 45)
+PARAM_KP = os.getenv('PARAM_KP', 0.0)
+PARAM_KI = os.getenv('PARAM_KI', 0.03)
+PARAM_KD = os.getenv('PARAM_KD', 0.0)
+PARAM_KPOM = os.getenv('PARAM_KPOM', 10.0)
+PARAM_POM_WEIGHT = os.getenv('PARAM_POM_WEIGHT', 0.05)
+
+temperature_offset = 0.5 # "Disable" Tado control algorithm
+flow_max_update_interval = timedelta(hours=1)
+flow_max = None
+flow_max_last_update = datetime.min
 
 if len(TADO_USERNAME) == 0 or len(TADO_PASSWORD) == 0:
     sys.exit("TADO_USERNAME and TADO_PASSWORD must be set")
@@ -24,48 +34,82 @@ def main():
     """Retrieve all zones, once successfully logged in"""
     tado = Tado(username=TADO_USERNAME, password=TADO_PASSWORD)
     flow = tado.get_flow_temperature_optimization()['maxFlowTemperature']
-    flow_max = 60 # TODO: calculate
-
-    zoneStates = tado.get_zone_states() # TODO: Do not execute twice on startup
-    z0 = zoneStates[0]
-    print(z0)
-    z1 = zoneStates[1]
-    pid0 = FlowController(name = z0['name'],
-                          Kp=0.0,
-                          Ki=0.03,
-                          Kd=0.0,
-                          Kpom=10.0,
-                          weightPom=0.05,
-                          fadePom=0.001,
-                          setpoint=z0['setting']['temperature']['value'],
-                          output_limits=(FLOW_MIN, flow_max),
-                          starting_output=flow)
-    pid1 = FlowController(name = z1['name'],
-                          Kp=0.0,
-                          Ki=0.03,
-                          Kd=0.0,
-                          Kpom=10.0,
-                          weightPom=0.05,
-                          fadePom=0.001,
-                          setpoint=z1['setting']['temperature']['value'],
-                          output_limits=(FLOW_MIN, flow_max),
-                          starting_output=flow)
+    controllers = {}
 
     while True:
+        update_flow_max(tado)
         zoneStates = tado.get_zone_states()
 
-        p0Flow = pid0.update(zoneStates[0])
-        p1Flow = pid1.update(zoneStates[1])
+        # Create or update a controller for each zone
+        for zone in zoneStates:
+            # Only execute for this zone if setting power is not OFF:
+            if zone['setting']['power'] == 'OFF':
+                continue
+            
+            setpoint = zone['setting']['temperature']['value'] - temperature_offset
+            current = zone['sensorDataPoints']['insideTemperature']['value']
+            id = zone['id']
+            name = zone['name']
 
-        new_flow = max(p0Flow, p1Flow)
-        new_flow_rounded = round(new_flow)
+            # Initialize controller in controllers identified by id if it does not exist
+            if id not in controllers:
+                controllers[id] = create_new_controller(name, setpoint, flow)
+            
+            controllers[id].update(setpoint, current)
+        
+        # Get maximum controller output for all controllers
+        max_output = max([controller.output for controller in controllers.values()])
+        max_output_rounded = round(max_output)
 
-        if new_flow_rounded != flow:
-            print(f"Setting flow to: {new_flow_rounded}")
-            tado.set_flow_temperature_optimization(new_flow_rounded)
-            flow = new_flow_rounded
+        # Print representation of all controllers in one line concatenated with |
+        print(" | ".join([str(controller) for controller in controllers.values()]))
+
+        # Update flow temperature optimization if necessary
+        if max_output_rounded != flow:
+            print(f"Setting flow to: {max_output_rounded}")
+            tado.set_flow_temperature_optimization(max_output_rounded)
+            flow = max_output_rounded
         
         time.sleep(60)
+
+def create_new_controller(name, setpoint, flow):
+    return FlowController(name = name,
+                          Kp = PARAM_KP,
+                          Ki = PARAM_KI,
+                          Kd = PARAM_KD,
+                          Kpom = PARAM_KPOM,
+                          weightPom = PARAM_POM_WEIGHT,
+                          setpoint = setpoint,
+                          output_limits=(FLOW_MIN, flow_max),
+                          starting_output=flow)
+
+def update_flow_max(tado):
+    global flow_max_last_update
+    global flow_max_update_interval
+    global flow_max
+
+    # Return if last update was less than an hour ago
+    if datetime.now() - flow_max_last_update < flow_max_update_interval:
+        return
+    
+    outsideTemperature = tado.get_weather()['outsideTemperature']['celsius']
+    flow_max = _calc_flow_max(outsideTemperature)
+    flow_max_last_update = datetime.now()
+    print(f"Calculated flow_max: {flow_max}")
+
+def _calc_flow_max(outsideTemperature):
+    a_x = -10
+    a_y = FLOW_MAX_MINUS10
+    b_x = 20
+    b_y = FLOW_MAX_PLUS20
+    flow = a_y + (b_y - a_y) / (b_x - a_x) * (outsideTemperature - a_x)
+    
+    if flow < FLOW_MAX_PLUS20:
+        return FLOW_MAX_PLUS20
+    if flow > FLOW_MAX_MINUS10:
+        return FLOW_MAX_MINUS10
+    
+    return round(flow)
 
 if __name__ == "__main__":
     main()
--- flowController.py
+++ flowController.py
@@ -2,42 +2,33 @@ from datetime import datetime
 from PyTado.interface.interface import Tado
 from simple_pid import PID
 
-tempOffset = 0.5 # "Disable" Tado control algorithm
-
 class FlowController:
-    def __init__(self, name, Kp, Ki, Kd, Kpom, weightPom, fadePom, setpoint, output_limits, starting_output):
+    def __init__(self, name, Kp, Ki, Kd, Kpom, weightPom, setpoint, output_limits, starting_output):
         self.name = name
-        self.Kp = Kp
-        self.Ki = Ki
-        self.Kd =Kd
-        self.Kpom = Kpom
-        self.weightPom = weightPom
-        self.fadePom = fadePom
-        self.setpoint = setpoint
-        self.output_limits = output_limits
-        self.starting_output = starting_output
-
-        self.pid = PID(Kp=0.0,
-                       Ki=0.03,
-                       Kd=0.0,
-                       Kpom=10.0,
-                       weightPom=0.05,
-                       fadePom=0.001,
+        self.output = starting_output
+        self.pid = PID(Kp=Kp,
+                       Ki=Ki,
+                       Kd=Kd,
+                       Kpom=Kpom,
+                       weightPom=weightPom,
                        setpoint=setpoint,
                        sample_time=None,
                        output_limits=output_limits,
                        starting_output=starting_output)
         
-    def update(self, zoneState):
-        target = zoneState['setting']['temperature']['value'] - tempOffset
-        current = zoneState['sensorDataPoints']['insideTemperature']['value']
+    def update(self, setpoint, current):
+        self.pid.setpoint = setpoint
+        self.current = current
+        self.output = self.pid(current)
+    
+    def __repr__(self):
+        components = self.pid.components
+        p = components[0]
+        i = components[1]
+        d = components[2]
+        pom = components[3]
 
-        self.pid.setpoint = target
-        new_flow = self.pid(current)
-
-        print(f"{datetime.now().replace(microsecond=0).isoformat()} {self.name} ({current:.2f} {target:.2f}) ({new_flow:.2f}) {self.pid.components}")
-
-        return new_flow
+        return f"{self.name} {self.current:.2f} {self.pid.setpoint:.2f} {self.output:.2f} [{p:.2f} {i:.2f} {d:.2f} {pom:.2f}]"
 
         
 
